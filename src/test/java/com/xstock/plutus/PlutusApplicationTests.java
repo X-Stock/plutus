a396c2f8fb19deb3f1e26c8bfd71aad3d544b651
Kim Minh <100175752+kim-minh@users.noreply.github.com>
2024-03-19 10:42:05 +0700

    Initial commit

---
+++ src/test/java/com/xstock/plutus/PlutusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xstock.plutus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlutusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
