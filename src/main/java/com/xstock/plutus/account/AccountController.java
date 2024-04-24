3bfbb33766c4996ea1f73f9128e677b1c288be7e
Tuan Nguyen <nguyentuan123.yeah@gmail.com>
2024-04-24 11:42:31 +0700

    Create Account for Api Testing

---
+++ src/main/java/com/xstock/plutus/account/AccountController.java
@@ -0,0 +1,38 @@
+package com.xstock.plutus.account;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping
+    public Account createAccount(@RequestBody Account account) {
+        return accountService.createAccount(account);
+    }
+
+    @GetMapping
+    public List<Account> getAllAccounts() {
+        return accountService.getAllAccounts();
+    }
+
+    @GetMapping("/{id}")
+    public Account getAccountById(@PathVariable Long id) {
+        return accountService.getAccountById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Account updateAccount(@PathVariable Long id, @RequestBody Account account) {
+        return accountService.updateAccount(id, account);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAccount(@PathVariable Long id) {
+        accountService.deleteAccount(id);
+    }
+}
