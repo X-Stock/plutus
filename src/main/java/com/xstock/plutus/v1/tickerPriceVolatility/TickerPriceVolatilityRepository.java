21af27228b9a193a4993e94641752a3384f865b0
Kim Minh <100175752+kim-minh@users.noreply.github.com>
2024-06-28 12:46:16 +0700

    Remove TickerPriceVolatility entity

--- src/main/java/com/xstock/plutus/v1/tickerPriceVolatility/TickerPriceVolatilityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.xstock.plutus.v1.tickerPriceVolatility;
-
-import com.xstock.plutus.utils.interfaces.CommonRepository;
-
-public interface TickerPriceVolatilityRepository extends CommonRepository<TickerPriceVolatility> {
-}
