40e4e61c18a3d90e381b3750f27266b92a147513
Kim Minh <100175752+kim-minh@users.noreply.github.com>
2024-07-31 18:58:42 +0700

    Add missing database constraint

--- src/main/java/com/xstock/plutus/v1/company/Company.java
+++ src/main/java/com/xstock/plutus/v1/company/Company.java
@@ -5,6 +5,8 @@ import com.xstock.plutus.v1.stockIndex.StockIndex;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.util.Set;
 
@@ -19,6 +21,7 @@ public class Company {
     private Integer id;
 
     @ManyToMany(mappedBy = "company")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Set<StockIndex> stockIndices;
 
     @Column(columnDefinition = "TEXT", nullable = false)
