7efad37905d28dbf8e1d5c7c63fefd6aba452476
Kim Minh <100175752+kim-minh@users.noreply.github.com>
2024-11-07 17:53:27 +0700

    Update Security Config to allow defined IPs

--- src/main/java/com/xstock/plutus/config/WebSecurityConfig.java
+++ src/main/java/com/xstock/plutus/config/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.xstock.plutus.config;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,16 +10,44 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
 public class WebSecurityConfig {
+    private static final Log log = LogFactory.getLog(WebSecurityConfig.class);
+
+    @Value("${spring.allow-ips:}")
+    private String[] allowIps;
+
+    private static final List<IpAddressMatcher> baseAllowedIps = List.of(
+            new IpAddressMatcher("127.0.0.1"),
+            new IpAddressMatcher("::1")
+    );
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        List<IpAddressMatcher> allowedIps = new ArrayList<>(baseAllowedIps);
+        if (allowIps.length > 0) {
+            log.warn(allowIps);
+            try {
+                for (String ip : allowIps) {
+                    allowedIps.add(new IpAddressMatcher(ip));
+                }
+            } catch (IllegalArgumentException e) {
+                log.warn(e.getMessage());
+            }
+        }
+        IpAddressMatcher[] ipAddressMatchers = allowedIps.toArray(new IpAddressMatcher[0]);
+
         http
+                .csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/actuator/**", "/docs/**")
+                        .requestMatchers(ipAddressMatchers)
                         .permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
